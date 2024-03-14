786621c91e45dc146c730fd216f694d9f813dc0e
Suur Toss <sander.orav@voco.ee>
2024-03-14 10:40:35 +0200

    Add project basic file

---
+++ game.py
@@ -0,0 +1,14 @@
+import pygame
+pygame.init()
+
+screen = pygame.display.set_mode([800, 500])
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.flip()
+
+pygame.quit()
